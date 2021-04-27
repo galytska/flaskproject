9267adfe6ace19fcbaa9db7e55a9ee1b8ce6edc5
FGVMdizT <galytska13@gmail.com>
2021-04-27 08:52:10 +0300

    add test smoke case

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'youneverknow'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL').replace('postgres://',
-                                                                                            'postgresql://') or 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    environ.get('DATABASE_URL').replace('postgres://','postgresql://')  if environ.get('DATABASE_URL') else 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
