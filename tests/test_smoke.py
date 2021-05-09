8700ecbdbe56c33cbb6a1446d0da6937c30f657d
FGVMdizT <galytska13@gmail.com>
2021-05-09 13:16:55 +0300

    try to add code coverage

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -11,3 +11,13 @@ def test_status_code(client):
     """
     return_value = client.get('/')
     assert return_value.status_code == 200
+
+
+def test_status_code2(client):
+    """
+    Check status code
+    :param client: client fixture
+    :return: bool
+    """
+    return_value = client.get('/news/1')
+    assert return_value.status_code == 200
