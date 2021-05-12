aa798f8ffbc329617871143f6391d8aa1064e2ae
FGVMdizT <galytska13@gmail.com>
2021-05-12 16:57:30 +0300

    add post test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -43,10 +43,17 @@ def test_status_login(client):
     assert return_value.status_code == 200
 
 
-def test_empty_db(client):
+def test_correct_template(client):
     """Start with a blank database."""
 
     rv = client.get('/')
     assert b'Good news' in rv.data
 
 
+def test_messages(client):
+    """Test that messages work."""
+    rv = client.post('/', data=dict(
+        news='Hello',
+        news_text='allowed here'
+    ), follow_redirects=True)
+    assert rv.status_code == 200
