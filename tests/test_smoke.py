1bfb6a6a89d436b3f5b60e5ababfc6992f8c03e8
FGVMdizT <galytska13@gmail.com>
2021-05-13 16:57:47 +0300

    add invalid register test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -83,3 +83,16 @@ def test_messages(client):
         news_text='allowed here'
     ), follow_redirects=True)
     assert response.status_code == 200
+
+
+def test_invalid_register_post(client):
+    """Test invalid message."""
+    fake_data = 1
+    response = client.post('/register', data=dict(
+        username=fake_data,
+        email=fake_data,
+        surname=fake_data,
+        password=fake_data
+
+    ), follow_redirects=True)
+    assert b'Invalid email address' in response.data
